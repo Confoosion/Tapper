3e0bd834b239cd8bfda584bb72016c10f6075b67
Jake <jakesales@gmail.com>
2025-09-09 17:27:12 -0700

    Added leaderboard package
    Planning to add a Unity leaderboard to replace the "Themes" button

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference TapperLeaderboard = new LeaderboardReference("fae9568c27d3987720c81b07b6c34bcf3ef2a0102dd4420ad4eb399f6a39ccec ");
+    }
+}
