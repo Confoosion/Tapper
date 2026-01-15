0eea19a38ee61ae5a79464fc6e7e9b91c9efdd2f
Jake Sales <jakesales@gmail.com>
2026-01-14 19:06:28 -0800

    Created Animal Set shop category
    No functionality on buying/equipping yet, but can window shop. The other two categories do not work yet.

--- Assets/ScriptableObjects/AnimalSet_SO.cs
+++ Assets/ScriptableObjects/AnimalSet_SO.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AnimalSet_SO", menuName = "Scriptable Objects/Themes/AnimalSet_SO")]
 public class AnimalSet_SO : ScriptableObject
 {
+    public bool isUnlocked = false;
+    public bool isEquipped = false;
+
+    [Space]
+
+    [Header("Shop Preview")]
+    public Sprite preview_BG;
+    public Sprite preview_Set;
+    public int preview_Price;
+
+    [Space]
+
+    [Header("Animal Set")]
     public GameObject[] goodTargets = new GameObject[3];
     public GameObject badTarget;
 }
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,22 +1,78 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
     public enum ShopCategory { Animals, Backgrounds, Taps }
-    [SerializeField] ShopCategory currentShopType;
+    public enum ShopButtonState { Locked, Unlocked, Equipped }
+
+    public ShopCategory currentShopType;
+    public ShopButtonState currentShopState;
 
     [Space]
-    [SerializeField] AnimalSet_SO[] animalSets; 
 
-    private void UpdateShopVisuals()
+    [Header("Shop UI")]
+    [SerializeField] private Image animal_BG;
+    [SerializeField] private Image animal_Set;
+
+    [SerializeField] private Image shopButton;
+    [SerializeField] private Sprite[] shopButtonSprites;
+    [SerializeField] private TextMeshProUGUI shopTitle;
+    [SerializeField] private TextMeshProUGUI shopCost;
+
+    [Space]
+    [SerializeField] private int currentShopIndex = 0;
+    private int MAX_ShopIndex;
+    [SerializeField] private AnimalSet_SO[] animalSets;
+
+    // SHOP CATEGORY
+    private void UpdateShopCategoryVisuals(int index)
     {
-        
+        string title;
+        int price;
+
+        switch(currentShopType)
+        {
+            case ShopCategory.Animals:
+                {
+                    animal_BG.sprite = animalSets[index].preview_BG;
+                    animal_Set.sprite = animalSets[index].preview_Set;
+                    
+                    shopTitle.SetText(animalSets[index].name);
+                    shopCost.SetText(animalSets[index].preview_Price.ToString());
+
+                    if(animalSets[index].isUnlocked)
+                    {
+                        if(animalSets[index].isEquipped)
+                            currentShopState = ShopButtonState.Equipped;
+                        else
+                            currentShopState = ShopButtonState.Unlocked;
+                    }
+                    else
+                        currentShopState = ShopButtonState.Locked;
+
+                    MAX_ShopIndex = animalSets.Length;
+
+                    break;
+                }
+            case ShopCategory.Backgrounds:
+                {
+                    break;
+                }
+            case ShopCategory.Taps:
+                {
+                    break;
+                }
+        }
+
+        UpdateShopButton();
     }
 
     private void SwitchShopCategory(ShopCategory shopType)
     {
         currentShopType = shopType;
-        UpdateShopVisuals();
+        UpdateShopCategoryVisuals(0);
     }
 
     public void GoToAnimalsCategory()
@@ -32,5 +88,63 @@ public class ShopManager : MonoBehaviour
     public void GoToTapsCategory()
     {
         SwitchShopCategory(ShopCategory.Taps);
+    }
+
+    // SHOP MOVEMENT
+    public void UpdateShopButton()
+    {
+        switch(currentShopState)
+        {
+            case ShopButtonState.Locked:
+                {
+                    shopButton.sprite = shopButtonSprites[0];
+                    break;
+                }
+
+            case ShopButtonState.Unlocked:
+                {
+                    shopButton.sprite = shopButtonSprites[1];
+                    break;
+                }
+
+            case ShopButtonState.Equipped:
+                {
+                    shopButton.sprite = shopButtonSprites[2];
+                    break;
+                }
+        }
+    }
+
+    public void ShopLeft()
+    {
+        currentShopIndex--;
+        if(currentShopIndex < 0)
+        {
+            currentShopIndex = MAX_ShopIndex - 1;
+        }
+
+        UpdateShopCategoryVisuals(currentShopIndex);
+    }
+
+    public void ShopRight()
+    {
+        currentShopIndex++;
+        if(currentShopIndex > MAX_ShopIndex)
+        {
+            currentShopIndex = 0;
+        }
+
+        UpdateShopCategoryVisuals(currentShopIndex);
+    }
+
+    // BUYING/EQUIPING
+    public void BuyItem()
+    {
+        
+    }
+
+    public void EquipItem()
+    {
+        
     }
 }
