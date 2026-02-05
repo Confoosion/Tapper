fb6eac6a31049bed7bcfc7579be535b57174b03a
Jake Sales <jakesales@gmail.com>
2026-02-05 11:38:30 -0800

    Start screen bug fixed
    Sometimes if consistently tapping the mole before and after it appears, it would not count as a "tap" and bring you to the main menu

--- Assets/Scripts/MoleStartButton.cs
+++ Assets/Scripts/MoleStartButton.cs
@@ -12,6 +12,7 @@ public class MoleStartButton : MonoBehaviour, IPointerDownHandler
     [SerializeField] private GameObject tapStart_GO;
 
     private float delay = 1.5f;
+    // private float waitTapTime = 0.3f;
 
     void Awake()
     {
@@ -37,14 +38,14 @@ public class MoleStartButton : MonoBehaviour, IPointerDownHandler
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (!LeanTween.isTweening(this.gameObject) && !ScreenManager.Singleton.IsTransitionGoing())
+        if (!ScreenManager.Singleton.IsTransitionGoing())
         {
             UIManager.Singleton.HideSettingsAndPauseIcon();
         
             tapImage.gameObject.SetActive(true);
 
             SoundManager.Singleton.PlayHitSound();
-            targetAnimation.QueueHitAnimation();
+            targetAnimation.StartHitAnimation();
             // targetAnimation.StartHitAnimation();
         }
     }
