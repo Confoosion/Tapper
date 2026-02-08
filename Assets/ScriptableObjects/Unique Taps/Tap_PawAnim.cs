65b2c318ad613b76d5a287732663ad90f45aa5b0
Jake Sales <jakesales@gmail.com>
2026-02-08 14:56:26 -0800

    Cat paw Tap WIP
    Think it still needs a bit of tweaking and needs a finish

--- Assets/ScriptableObjects/Unique Taps/Tap_PawAnim.cs
+++ Assets/ScriptableObjects/Unique Taps/Tap_PawAnim.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tap_PawAnim : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        PlayAnim();    
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Image tapImage;
+    [SerializeField] private float radius;
+    [SerializeField] private float animTime;
+    private Vector3 centerPos;
+    public void PlayAnim()
     {
-        
+        centerPos = transform.position;
+
+        Vector3 randomPoint = Random.insideUnitCircle.normalized * radius;
+        transform.position = centerPos + randomPoint; // Randomize position
+
+        Vector3 direction = centerPos - transform.position; // Change rotation towards center
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+
+        tapImage.enabled = true;
+
+        LeanTween.move(this.gameObject, centerPos, animTime).setEase(LeanTweenType.easeOutCirc);
     }
 }
