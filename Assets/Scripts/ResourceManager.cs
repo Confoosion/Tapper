67207fe9274d4bbb55cb41f3047ced7686e8257a
Jake Sales <jakesales@gmail.com>
2026-02-14 18:46:02 -0800

    Removed old assets
    Removed a lot of version 1 assets that are no longer needed. Also fixed the Desert animal set ScriptableObject

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -7,8 +7,6 @@ using System.Linq;
 public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager Singleton { get; private set; }
-    public Sprite[] goodCircle_Sprites;
-    public Sprite[] badCircle_Sprites;
 
     public Dictionary<SoundType, AudioClip[]> SOUNDS = new Dictionary<SoundType, AudioClip[]>();
 
@@ -22,9 +20,6 @@ public class ResourceManager : MonoBehaviour
 
     void RetrieveResources()
     {
-        goodCircle_Sprites = Resources.LoadAll("Good_Circles", typeof(Sprite)).Cast<Sprite>().ToArray();
-        badCircle_Sprites = Resources.LoadAll("Bad_Circles", typeof(Sprite)).Cast<Sprite>().ToArray();
-
         foreach (SoundType soundType in Enum.GetValues(typeof(SoundType)))
         {
             SOUNDS.Add(soundType, Resources.LoadAll("Sounds/" + soundType.ToString(), typeof(AudioClip)).Cast<AudioClip>().ToArray());
